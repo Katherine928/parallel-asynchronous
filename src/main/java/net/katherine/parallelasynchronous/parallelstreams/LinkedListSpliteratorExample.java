e7fc415ba2677e354fc6dae1c034629c847216b8
Katherine928 <katherine71317@gmail.com>
2023-06-23 15:47:44 -0400

    Spliterator Performance

---
+++ src/main/java/net/katherine/parallelasynchronous/parallelstreams/LinkedListSpliteratorExample.java
@@ -0,0 +1,29 @@
+package net.katherine.parallelasynchronous.parallelstreams;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static net.katherine.parallelasynchronous.util.CommonUtil.*;
+
+public class LinkedListSpliteratorExample {
+
+
+    public List<Integer> multiplyEachValue(LinkedList<Integer> inputList, int multiplyValue, boolean isParallel) {
+
+        startTimer();
+        Stream<Integer> integerStream = inputList.stream(); // sequential
+
+        if(isParallel) {
+            integerStream.parallel();
+        }
+
+        List<Integer> resultList = integerStream
+                .map(integer -> integer*multiplyValue)
+                .toList();
+        timeTaken();
+        stopWatchReset();
+        return resultList;
+    }
+
+}
